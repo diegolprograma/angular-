0de4c27fa5a2791f55c97cf91e07786ab1b53001
diegolmusico <you@example.com>
2024-08-17 03:30:40 -0300

    angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { IndexComponent } from './components/index/index.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SobreMiComponent,
+    ExperienciaComponent,
+    SkillsComponent,
+    IndexComponent,
+    ProyectosComponent,
+    FooterComponent,
+   
   ],
   imports: [
     BrowserModule,
